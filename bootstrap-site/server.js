3658fc0ecef27a64bbe88e3bd19f3fde86e7d5b0
Oscar Björkgren <osbjorkg@abo.fi>
2020-01-07 14:22:38 +0200

    Fixed search bar collapse animation. Added node JS test server script.

---
+++ bootstrap-site/server.js
@@ -0,0 +1,10 @@
+/* 
+	Simple node connect server
+	$ node server.js
+*/
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
